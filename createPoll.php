2820691d7ece026ca08f8e37330f380975c40400
jwyatt2 <julian.c.wyatt1@gmail.com>
2015-10-08 23:34:34 -0400

    "Create Poll" Pages
    They aren't pretty, but here's a Poll Creation tool to test out
    inserting polls into the database. Open the createPoll.php page to get
    user input, click submit, and the createPollBackend.php will concatenate
    the user input into a string and attempt to insert it into the database.
    
    When inserting there is currently an error with the Poll ID

---
+++ createPoll.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<!-- basic poll creation page
+
+The first line below, where action = "xxxx" is the page that will
+load when submit is pressed. -->
+	
+	<form action="createPollBackend.php" method="post">
+	Enter in a question: <input type="text" name="quest"><br>
+	Answer One: <input type="text" name="ans1"><br>
+	Answer Two: <input type="text" name="ans2"><br>
+	Answer Three: <input type="text" name="ans3"><br>
+	Answer Four: <input type="text" name="ans4"><br>
+	Answer Five: <input type="text" name="ans5"><br>
+	Answer Six: <input type="text" name="ans6"><br>
+	<input type="submit">
+	</form>
+
+<body>
+<html>
