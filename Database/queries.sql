e72b02c40ae02bf3b9028ddf8274bb631c7fb6c0
Alexander <alexsniffin@hotmail.com>
2015-10-12 18:38:00 -0400

    Added in Random Public and Private poll procedures

--- Database/queries.sql
+++ Database/queries.sql
@@ -5,24 +5,35 @@
 -- Get new random public poll for some <USER_ID>
 -- Projects all of the polls that the <USER_ID> doesn't own, and also checks
 -- all of the polls in which the user has voted in. Will check the date to close attribute too.
-SELECT P_ID
-FROM Polls p
-WHERE p.User_ID <> <USER_ID> AND p.share_code IS NULL AND date_to_close > NOW() AND NOT EXISTS (
-	SELECT User_ID
-	FROM Voted v
-	WHERE v.User_ID = <USER_ID> AND v.ANS_ID IN (
-		SELECT ANS_ID
-		FROM Answers a
-		WHERE a.P_ID = p.P_ID
+DELIMITER //
+CREATE PROCEDURE random_public_poll
+	(IN user INT)
+BEGIN
+	SELECT P_ID
+	FROM Polls p
+	WHERE p.User_ID <> user AND p.share_code IS NULL AND date_to_close > NOW() AND NOT EXISTS (
+		SELECT User_ID
+		FROM Voted v
+		WHERE v.User_ID = user AND v.ANS_ID IN (
+			SELECT ANS_ID
+			FROM Answers a
+			WHERE a.P_ID = p.P_ID
+		)
 	)
-);
+	ORDER BY RAND() LIMIT 1;
+END //
+DELIMITER;
+--
+CALL public_Poll(<id>);
 
 -- #2
 -- Get a Private Poll using a <share_code>
-SELECT P_ID
-FROM Polls p
-WHERE p.share_code = '<SHARE_CODE>';
-
--- #3
--- Creating a Poll Procedure
--- TO-DO
+DELIMITER //
+CREATE PROCEDURE private_poll
+	(IN share_code CHAR(8))
+BEGIN
+	SELECT P_ID
+	FROM Polls p
+	WHERE p.share_code = share_code;
+END //
+DELIMITER;
