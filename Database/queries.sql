6b87d885f4578e247b44d342421afd6ae163de7f
Alexander <alexsniffin@hotmail.com>
2015-10-13 23:00:56 -0400

    Added in getRandomPublicPoll function, getPrivatePoll function, getPollQuestion procedure, getPollAnswers procedure. See Database/queries.sql for details.

--- Database/queries.sql
+++ Database/queries.sql
@@ -25,7 +25,34 @@ BEGIN
 END //
 DELIMITER;
 -- Use
-CALL random_public_poll(<id>);
+CALL random_public_poll(<id>); -- returns a result set
+
+-- OR Try using the same, but as a function. It should only return a single value.
+DELIMITER //
+CREATE FUNCTION getRandomPublicPoll(user INT) 
+	RETURNS INT
+	DETERMINISTIC
+BEGIN
+	DECLARE poll_id varchar(10);
+
+	SELECT P_ID INTO poll_id
+	FROM Polls p
+	WHERE p.User_ID <> user AND p.share_code IS NULL AND date_to_close > NOW() AND NOT EXISTS (
+		SELECT User_ID
+		FROM Voted v
+		WHERE v.User_ID = user AND v.ANS_ID IN (
+			SELECT ANS_ID
+			FROM Answers a
+			WHERE a.P_ID = p.P_ID
+		)
+	)
+	ORDER BY RAND() LIMIT 1;
+
+	RETURN (poll_id);
+END;
+DELIMITER;
+-- Use
+SELECT getRandomPublicPoll(<id>); -- returns a single value
 
 -- #2
 -- Get a Private Poll using a <share_code>
@@ -38,6 +65,25 @@ BEGIN
 	WHERE p.share_code = share_code;
 END //
 DELIMITER;
+
+-- OR Try using the same, but as a function. It should only return a single value.
+DELIMITER //
+CREATE FUNCTION getPrivatePoll(share_code CHAR(8)) 
+	RETURNS INT
+	DETERMINISTIC
+BEGIN
+	DECLARE poll_id varchar(10);
+
+	SELECT P_ID INTO poll_id
+	FROM Polls p
+	WHERE p.share_code = share_code;
+
+	RETURN (poll_id);
+END;
+DELIMITER;
+-- Use
+SELECT getPrivatePoll(<share_code>); -- returns a single value
+--TODO Check if user voted on private poll, or owns private poll
 
 -- #3
 -- Create a poll and the corresponding answers
@@ -118,9 +164,9 @@ DELIMITER;
 -- Use
 CALL create_poll(1, 'Some question using procedure', NULL, 30, 'Some answer using procedure', 'Another answer using procedure', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
--- #4 getPoll
+-- #4 getPollQuestion, returns a single tuple with the question, name of person who made poll, and date created
 DELIMITER //
-CREATE PROCEDURE getPoll
+CREATE PROCEDURE getPollQuestion
 	(IN poll_id INT,
 	IN share_code CHAR(8))
 BEGIN
@@ -130,7 +176,14 @@ BEGIN
 	SELECT p.question, u.display_name, p.date_created
 	FROM Polls p, Users u
 	WHERE p.P_ID = poll_id AND u.ID = user;
+END //
+DELIMITER;
 
+-- #5 getPollAnswers, returns a result set of all answers for the given poll
+DELIMITER //
+CREATE PROCEDURE getPollAnswers
+	(IN poll_id INT)
+BEGIN
 	SELECT ans
 	FROM Answers a
 	WHERE a.P_ID = poll_id;
