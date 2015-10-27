2d398834f8d1c130c09254bce5504bbf2d5e2d2e
Lee Steele <leesteeledesign@gmail.com>
2015-10-26 20:01:54 -0400

    Added expiration date field to Create Poll page -- I found this calendar online (http://webdesignandsuch.com/add-a-calendar-date-picker-to-a-form-with-jquery/), so I'm not sure how we get the result from it to put into the database. Using the new HTML5 "date" input field is out of the question if we want browser compatibility, so this will have to do for now.

--- HTML/createPoll.php
+++ HTML/createPoll.php
@@ -7,16 +7,19 @@
 		<meta charset = "utf-8">
 		<meta name = "viewport" content = "width = device-width, initial-scale = 1">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
   		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+  		
+  		<!-- For calendar input -->
+  		<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+  		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+  		
+  		
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
 	</head>
 	
 <body>
-
-<!-- basic poll creation page
-The first line below, where action = "xxxx" is the page that will
-load when submit is pressed. -->
 
 	<!-- Navbar -->
 	<nav class="navbar navbar-inverse">
@@ -59,8 +62,9 @@ load when submit is pressed. -->
 		<div class="red-divider">
 		</div>
 	
-		<!-- Private Poll Checkbox -->
 		<div class="create-container">
+		
+			<!-- Category selection -->
 			<div class="form-group">
   				<label for="categoryselection"><strong>Choose a category:</strong> </label>
   				<select class="form-control" id="categoryselection">
@@ -75,26 +79,43 @@ load when submit is pressed. -->
 					<option>Politics</option>
   				</select>
 			</div>
-		
-			<div class="checkbox">
-  			<label><input type="checkbox" value="">This is a private poll</label>
-			</div>
-		</div>
-		
-		<!-- Create poll form -->
-		<form action="createPollBackend.php" method="post" class="create-form">
-			<strong>Enter a question:</strong> <input type="text" name="quest"><br>
-			Choice One:  <input type="text" name="ans1"><br>
-			Choice Two:  <input type="text" name="ans2"><br>
-			Choice Three:  <input type="text" name="ans3"><br>
-			Choice Four:  <input type="text" name="ans4"><br>
-			Choice Five:  <input type="text" name="ans5"><br>
-			Choice Six:  <input type="text" name="ans6"><br>
 			
-			<input type="submit" class="submit-button">
-		</form>
+			<!-- Date selection -->
+			<div class="form-group">
+				<label for="exp_date"><strong>Expiration Date:</strong> </label>
+				<!-- Need to somehow get result of this and give to database -->
+				<input id="datepicker" class="calendar-input"/>
+			</div>
+			
+			<!-- Private Poll Checkbox -->
+			<div class="checkbox">
+  				<label><input type="checkbox" value="">This is a private poll</label>
+			</div>
+		
+			<!-- Create poll form -->
+			<form action="createPollBackend.php" method="post" class="create-form">
+				<strong>Enter a question:</strong> <input type="text" name="quest"><br>
+				Choice One:  <input type="text" name="ans1"><br>
+				Choice Two:  <input type="text" name="ans2"><br>
+				Choice Three:  <input type="text" name="ans3"><br>
+				Choice Four:  <input type="text" name="ans4"><br>
+				Choice Five:  <input type="text" name="ans5"><br>
+				Choice Six:  <input type="text" name="ans6"><br>
+			
+				<input type="submit" class="submit-button">
+			</form>
+			
+		</div>	
 
 	</div>
+	
+	<!-- Calendar input -->
+	<script> $(document).ready(function()
+	{
+		$("#datepicker").datepicker();
+	});
+	
+	</script>
 
 </body>
 
